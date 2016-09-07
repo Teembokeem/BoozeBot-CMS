559308660aa4fc52bb371383ab4a2e195096a252
myunghoon kim <myungtkim@gmail.com>
2016-09-07 15:14:06 -0700

    ionic changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
@@ -50,34 +50,3 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
-=======
-const gulp = require('gulp');
-const HubRegistry = require('gulp-hub');
-const browserSync = require('browser-sync');
-
-const conf = require('./conf/gulp.conf');
-
-// Load some files into the registry
-const hub = new HubRegistry([conf.path.tasks('*.js')]);
-
-// Tell gulp to use the tasks just loaded
-gulp.registry(hub);
-
-gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
-gulp.task('test', gulp.series('karma:single-run'));
-gulp.task('test:auto', gulp.series('karma:auto-run'));
-gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
-gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
-gulp.task('default', gulp.series('clean', 'build'));
-gulp.task('watch', watch);
-
-function reloadBrowserSync(cb) {
-  browserSync.reload();
-  cb();
-}
-
-function watch(done) {
-  gulp.watch(conf.path.src('app/**/*.html'), reloadBrowserSync);
-  done();
-}
->>>>>>> 9979ceaaf6b1dba1b1006e3f29ffa29482b37187
