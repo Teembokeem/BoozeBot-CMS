8cfe7681b2254e59a9461e56f4631c4373db67af
myunghoon kim <myungtkim@gmail.com>
2016-09-08 12:36:24 -0700

    adds config for order view module

---
+++ www/view-modules/orders/Config.orders.view.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict'
+
+
+    angular.module('Orders.view.module')
+
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('booze.orders', {
+                url: '^/orders',
+                templateUrl: '/view-modules/orders/orders.html',
+                controller: 'OrdersCtrl',
+                controllerAs: 'vc'
+            })
+
+    });
+
+})();
