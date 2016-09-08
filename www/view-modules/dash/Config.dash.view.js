09f3894852af4316499e0c16ce6d01b82cddd489
myunghoon kim <myungtkim@gmail.com>
2016-09-08 12:36:00 -0700

    adds dash view module config

---
+++ www/view-modules/dash/Config.dash.view.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict'
+
+
+    angular.module('Dash.view.module')
+
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('booze.dash', {
+                url: '^/dash',
+                templateUrl: '/view-modules/dash/dash.html',
+                controller: 'DashCtrl',
+                controllerAs: 'vc'
+            })
+
+    });
+
+})();
