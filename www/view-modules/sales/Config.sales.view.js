739d2a9a2c66500e872f14d003bf0dc5f1b5f874
myunghoon kim <myungtkim@gmail.com>
2016-09-08 15:45:42 -0700

    adds config for sales view module

---
+++ www/view-modules/sales/Config.sales.view.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict'
+
+
+    angular.module('Sales.view.module')
+
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('booze.sales', {
+                url: '^/sales',
+                templateUrl: '/view-modules/sales/sales.html',
+                controller: 'SalesCtrl',
+                controllerAs: 'vc',
+                contentTitle: 'Sales'
+            })
+
+    });
+
+})();
