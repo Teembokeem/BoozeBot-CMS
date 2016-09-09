5bc732c8de54cb583d72a71d0ed505067bef5f14
myunghoon kim <myungtkim@gmail.com>
2016-09-09 16:44:51 -0700

    does testing for how to set request methods

--- www/api-modules/api.base.module.js
+++ www/api-modules/api.base.module.js
@@ -4,3 +4,24 @@
   angular.module('Api.base.module', ['urlService', 'Services']);
   
 })();
+
+/*
+  function BaseApi(url, formatRes) {
+      this.options
+      this.url
+      return new APIModule()
+  }
+
+  function newMthod(start) {
+    return funtion(data) {
+      return start(this.url, data, this.options).then(format).catch(format)
+    }
+    
+    var get = newMethod($http.get)
+
+    function get(data) {
+      return $http.get(this.url, data, this.options).then(format).catch
+    }
+    get()
+  }
+*/
