cdf5f1956dee5e0b3c8a7641473f2e43f910f03a
myunghoon kim <myungtkim@gmail.com>
2016-09-09 16:47:35 -0700

    adds config for product view module

---
+++ www/view-modules/products/Config.products.view.js
@@ -0,0 +1,56 @@
+(function() {
+    'use strict'
+
+
+    angular.module('Products.view.module')
+
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('booze.products', {
+                url: '^/products',
+                templateUrl: '/view-modules/products/products.html',
+                controller: 'ProductsCtrl',
+                controllerAs: 'vc',
+                // resolve: {
+                //     products : function($q, ProductManager) {
+                //         console.log('Providing!')
+                //         var promise = $q.deferred()
+                //         if (ProductManager.Products.length) {
+                //             return $q.resolve(ProductManager.Products);
+                //         }
+                //         return ProductManager.getProducts();
+                //     }
+                // },
+                contentTitle: 'Products'
+            })
+
+            .state('booze.products.show', {
+                url: '^/:name',
+                // resolve: {
+                //     products : function($q, ProductManager) {
+                //         console.log('Providing!')
+                //         var promise = $q.deferred()
+                //         if (ProductManager.Products.length) {
+                //             return $q.resolve(ProductManager.Products);
+                //         }
+                //         return ProductManager.getProducts();
+                //     }
+                // },
+            })
+
+    })
+
+    // .config(function(ProductManagerProvider, $logProvider) {
+    //             $logProvider.$get().info("Grabbing products.");
+    //             ProductManagerProvider.$get().getProducts()
+    //                 .then(function(products) {
+    //                     $logProvider.$get().info("Success: received products");
+    //                 })
+    //                 .catch(function(err) {
+    //                     $logProvider.$get().info("Error: ", err)
+    //                 }) 
+    // })
+
+})();
