1e4cb743c7cb41d9868b987d9cc19ba1cc1d693c
myunghoon kim <myungtkim@gmail.com>
2016-09-12 15:43:08 -0700

    adds seed orders to api module

--- www/api-modules/order/Module.order.api.js
+++ www/api-modules/order/Module.order.api.js
@@ -3,11 +3,47 @@
 
   angular.module('Order.api.module', ['Api.base.module'])
     .run(function(OrderManager, storage) {
-      
+              
+        var seedOrders = [
+            {
+            name: 'Ben',
+            total: 35.00,
+            status: "PD" 
+            },
+            {
+            name: 'Adam',
+            total: 35.00,
+            status: "AS" 
+            },
+            {
+            name: 'Craig',
+            total: 35.00,
+            status: "AC" 
+            },
+            {
+            name: 'Ben',
+            total: 35.00,
+            status: "PD" 
+            },
+            {
+            name: 'Adam',
+            total: 35.00,
+            status: "IP" 
+            },
+            {
+            name: 'Craig',
+            total: 35.00,
+            status: "CP" 
+            }
+        ];
+
       OrderManager.store = new storage('orders');
       console.log('namespace', OrderManager.namespace)
       if (OrderManager.store.getItem()) {
         OrderManager.orders = OrderManager.store.getItem();
+      } else {
+        // OrderManager.getOrders();
+        OrderManager.orders = OrderManager.transformOrders(seedOrders);
       }
     });
 })();
